af566129b1cd48ce9d9ae9e7888e5da9af6b6ac4
Vladislav Saling <vladislavsaling@gmail.com>
2023-07-12 17:05:03 +0200

    build size testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      {/*<IconWrapper iconType="faSpinner" />*/}
       <IconWrapper iconType="faCoffee" />
       <h2>Start editing to see some magic happen!</h2>
     </div>
--- src/IconWrapper.js
+++ src/IconWrapper.js
@@ -1,15 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faCoffee, faSpinner, faRobot } from "@fortawesome/free-solid-svg-icons";
 
 export default function IconWrapper({ iconType, ...props }) {
   const iconT = () => {
     switch (iconType) {
       case "faCoffee":
         return faCoffee;
+      case "faSpinner":
+        return faSpinner;
+      case "faRobot":
+        return faRobot;
       default:
         return null;
     }
   };
-
+  
   return <FontAwesomeIcon icon={iconT()} />;
 }
